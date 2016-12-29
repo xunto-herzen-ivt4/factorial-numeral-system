25e15f44ff545412c60f029545b09db4eaf62c5a
Xunto <hunterdelyx1@gmail.com>
2016-12-29 14:05:16 +0300

    rewrite

--- factorial_ns.py
+++ factorial_ns.py
@@ -2,33 +2,22 @@ import math
 
 
 def convert(number):
-    if number == 0:
-        return [0]
-
-    factorials = []
-    next_fac = 1
-
-    tmp = 2
-    while next_fac <= number:
-        factorials.append(next_fac)
-        next_fac = factorials[-1] * tmp
-        tmp += 1
-
     result = []
-    for factorial in reversed(factorials):
-        if number >= factorial:
-            result.append(math.floor(number / factorial))
-            number %= factorial
-        else:
-            result.append(0)
 
-    return result
+    i = 2
+    while number != 0:
+        number, mod = divmod(number, i)
+        result.append(mod)
+        i += 1
+
+    result.reverse()
+    return result + [0]
 
 
 def convert_to_dec(array):
     result = 0
 
-    factorial, tmp = 1, 2
+    factorial, tmp = 1, 1
     for item in reversed(array):
         result += item * factorial
         factorial *= tmp
